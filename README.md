dd66f9b5f4f05d9cae708a789c8b791a724823cf
linwenjun <linwenjun@163.com>
2019-10-11 09:32:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Job requirements . 
+- fork this repository. 
+- Complete the job as required by 'index.js'. 
+- Submit the job to github and the github address back to the system. 
+
---
+++ index.js
@@ -0,0 +1,13 @@
+// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+var name = 'hello';
+// TODO write your code here...
+
+
+// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+var sentence = 'good afternoon, mr mike.';
+//TODO write your code here...
+
+
+// Complete the following procedure to extract the amount of money variable, output: 20.
+var money = '￥20';
+//TODO write your code here...
